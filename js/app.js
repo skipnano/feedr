2f568e10a51947cd172b1364586f3ff6bf20c0a2
Dean Gilroy <dean.gilroy@gmail.com>
2015-12-20 14:25:14 -0500

    refactored html, js, css

--- js/app.js
+++ js/app.js
@@ -11,19 +11,34 @@ var $slideBox = $('#searchInput');
 var $button = $('.btn');
 var $close = $('.closePopUp');
 
-// toggle the search input when icon is clicked
+// Toggle the search input when icon is clicked
 $search.on('click', function () {
 	$slideBox.slideToggle(500);
 });
 
 // Display popup when button is clicked. Binds the event to the .main class, so all buttons added via jquery will fire
-$('.main').on('click', '.btn', function () {
+$('.main').on('click', '.btn', function (e) {
+	e.preventDefault();
 	$('#popUp').removeClass();
+	// debugger;
 	$('#searchIcon').hide();
+	// These get the data from teh parent element and save them to a varibale
+	var title = $(this).parent().find("h2").html();
+	var image = $(this).parent().find("img").attr('src');
+	var sourceLink = $(this).parent().find("p").html();
+	var comments = $(this).parent().find("h3").html();
+	// these inject the data from the variables above into the popup
+	$('#popUp').find("h1").html(title);
+	$('#popUp').find("img").attr("src", image);
+	$('#popUp').find("a").attr("href", sourceLink);
+	$('#popUp').find("p").html(comments +  " comments");
+	// this will scroll the windown to top so user can see the popup
+	$('body').scrollTop(0);
 });
 
 // Hide the popup when the X is clicked. Binds the event to the #popUp ID
-$('#popUp').on('click', '.closePopUp', function () {
+$('#popUp').on('click', '.closePopUp', function (e) {
+	e.preventDefault();
 	$('#popUp').addClass('hidden');
 	$('#searchIcon').show();
 });
@@ -33,7 +48,7 @@ $(document).ajaxStart(function() {
     $('.article-row').hide();
     $('#loader').css('display', 'block');
 });
-
+// Hides the ajax loader when the call is complete
 $(document).ajaxComplete(function(){
     $('#loader').css('display', 'none');
     $('.article-row').show();
@@ -44,32 +59,28 @@ $(document).ajaxComplete(function(){
 $.ajax ({
 	url: "https://www.reddit.com/top.json",
 	dataType: "json",
-	timeout: 5000,
+	// timeout: 5000,
 	success: function(response) {
 		console.log(response);
+
 		var redditResponse = response;
 		// This iterates over the returned data and adds elements from the object to the HTML
 		$(redditResponse.data.children).each(function () {
 			var counter = counter || 1;
-			$('.module').append('<img class="article-image" src=' + this.data.url + '/>');
-			$('.module').append('<h2>' + this.data.title + '</h2>');
-			$('.module').append("<button type='submit' value='submit' class='btn'>Read More</button>");
-			$('.module').append('<hr>');
+			// $('.article-column').append('<img class="article-image" src=' + this.data.url + '/>');
+			// $('.article-column').append('<h2>' + this.data.title + '</h2>');
+			// $('.article-column').append("<button type='submit' value='submit' class='btn'>Read More</button>");
+			// $('.article-column').html('</div>');
+			// $('.module').append('<hr>');
+			$('.main').append(articleDiv(this));
 			counter += 1;
 
-		if(counter === 3) {
-			$('.module').html('</div>');
-			$('.main').html('<div class="row article-row">');
-			$('.main').html('<div class="col-sm-4 article-column">');
-			$('.main').html('<div class="module">');
+		if(counter === 1) {
+			// $('.main').html('<div class="row article-row"></div>');
+			// $('.main').html('<div class="col-sm-4 article-column"></div>');
+			// $('.main').html('<div class="module"></div>');
 			counter = null;
 			}
-
-			// $('.main').on('click', '.btn', function() {
-			// 	var title = this.data.title;
-			// 	var description = this.data.media.oembed.description;
-			// 	console.log(description);
-			// });
 		});
 	},
 	error: function() {
@@ -77,18 +88,16 @@ $.ajax ({
 	}
 });
 
+// This function builds the html structure that will be appended in main
+function articleDiv(article) {
+    return '<div class="module">' +
+            '<img src="' + article.data.thumbnail + '"/>' +
+            '<h2>' + article.data.title + '</h2>' +
+			"<button type='submit' value='submit' class='btn'>Read More</button>" +
+			"<p class='hidden'>" + article.data.url + "</p>" +
+			"<h3 class='hidden'>" + article.data.num_comments + "</h3>" +
+			'</div>' 
+}
 
-// THIS IS PSEUDO CODE TO ITERATE OVER RETIRNED DATA AND ADD MODULES TO THE HTML WITH THAT DATA.
-/*
-$(redditResponse).each(function () {
-	var counter = counter || 1;
-	$('.module').html(this.title);
-	counter += 1;
 
-	if(counter === 3) {
-		$('.module').html('</div>');
-		$('.module').html('div class = row');
-		counter = null;
-	}
-});
-*/
+
